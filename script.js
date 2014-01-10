220e13c79ef2fc758aeb2f33101f5a68f725c606
MrDanMacDonald <dan@danmacd.com>
2014-01-10 23:07:56 +0000

    test variable working

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+	var button = $("<button>Delete</button>");
+	var newItem = $("<li>Test</li>");
 	$("input[type='text']").change(function(){
-		$(".todoList").append("<li>Test</li>");
+		$(".todoList").append(newItem);
 	});
 });
+
+
 
 
 
@@ -13,7 +17,8 @@ $(document).ready(function(){
 User enters newItem (text) into customBox (text box).
 
 When user clicks "add" button, newItem get appended to ol as a new ordered list item.
-The text then clears from the textbox.
+The text then clears from the textbox (can we wrap something in a form)
+
 When a newItem gets appended to ol, a delete button is also added beside each newItem
 
 When a user clicks on the deleteButton, the newItem closest to that deleteButton dissappears. */
