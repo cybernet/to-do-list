cf3cd470b2256c2efa22036069ac53f836659612
MrDanMacDonald <dan@danmacd.com>
2014-01-10 21:50:00 +0000

    Added pseudo code

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-	
+
 })
+
+
+/* PSUEDO CODE
+
+User enters newItem (text) into customBox (text box).
+
+When user clicks "add" button, newItem get appended to ol, and the text clears from the textbox
+
+When a newItem gets appended to ol, a delete button is also added beside each newItem
+
+When a user clicks on the deleteButton, the newItem closest to that deleteButton dissappears.
+
