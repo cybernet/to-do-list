5e0cfcdba0a52f5517945a7f88f2a09622b4bb04
MrDanMacDonald <dan@danmacd.com>
2014-01-11 04:14:20 +0000

    eliminated repetition of code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-	$("#addButton").click(function(event){
-		$(".todoList").append("<li>" + $("#customBox").val() + "<button>Delete</button>" + "</li>");
-		$("#customBox").val('');
-	});
 	$("form").submit(function(event){
 		event.preventDefault();
 		$(".todoList").append("<li>" + $("#customBox").val() + "<button>Delete</button>" + "</li>");
 		$("#customBox").val('');
 	}); 
+	$("button").click(function(event){
+		$(this).closest("li").remove();
+	});
 });
 
 
@@ -15,19 +14,5 @@ $(document).ready(function(){
 
 
 
-/* PSUEDO CODE
 
-User enters newItem (text) into customBox (text box).
-
-When user clicks "add" button, newItem get appended to ol as a new ordered list item.
-The text then clears from the textbox (can we wrap something in a form)
-
-When a newItem gets appended to ol, a delete button is also added beside each newItem
-
-When a user clicks on the deleteButton, the newItem closest to that deleteButton dissappears. 
-
-
-FIX ME
-
-textbox does not clear after entering*/
 
