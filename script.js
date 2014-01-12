0c347cc3ef2d5654f85938a593a96e24403662a4
MrDanMacDonald <dan@danmacd.com>
2014-01-12 00:31:31 +0000

    Edit button added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("form").submit(function(event){
 		event.preventDefault();
-		$(".todoList").append("<li>" + $("#customBox").val() + "<button>Delete</button>" +  "<input type='checkbox'>" + "</li>");
+		$(".todoList").append("<li>" + $("#customBox").val() + "<button>Delete</button>" + "<button>Edit</button>" + "<input type='checkbox'>" + "</li>");
 		$("#customBox").val('');
 	}); 
 	$(".todoList").on("click", "button", function(){
@@ -9,7 +9,6 @@ $(document).ready(function(){
 	});
 	$(".todoList").on("click", ":checkbox", function (){
 		$(this).parent().toggleClass("completedItem");
-
 	});
 });
 
@@ -18,5 +17,18 @@ $(document).ready(function(){
 
 
 
+/*
 
+#1 Add an edit button
+
+#2 When user clicks eddit, the value of the list item becomes editable text.
+
+#3 Hittin enter should turn the input into a regular li
+Prompt the user for the new value in a modal,
+or inline. Do not attempt to use the same textbox that is used to add todos for this functionality.
+Pre­populate the edit textbox with the original text.
+
+
+
+*/
 
