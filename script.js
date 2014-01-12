8ffc0a1d27c607482f44fb1fc4ba44dbdc4eec4a
MrDanMacDonald <dan@danmacd.com>
2014-01-12 21:20:22 +0000

    edit is now working properly

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-	$("form").submit(function(event){
+	$("form#mainInputBox").submit(function(event){
 		event.preventDefault();
 		var deleteButton = "<button class='delete'>Delete</button>";
 		var editButton = "<button class='edit'>Edit</button>";
-		$(".todoList").append("<li>" + $("#customBox").val() + deleteButton + editButton + "<input type='checkbox'>" + "</li>");
+		$(".todoList").append("<li>" + "<div>" + $("#customBox").val() + "</div>" + deleteButton + editButton + "<input type='checkbox'>" + "</li>");
 		$("#customBox").val('');
 	});
 	$(".todoList").on("click", "button.delete", function(){
 		$(this).parent().remove();
 	});
 	$(".todoList").on("click", "button.edit", function (){
-		var editItemBox = "<input type='text' class='itemBox'>";
-		$(this).parent().replaceWith("<form>" + editItemBox + "</form>");
+		var editItemBox = "<form class='editInputBox'><input type='text' class='itembox'></form>";
+		$(this).parent().replaceWith(editItemBox);
+		$("form.editInputBox ").on("submit", function(){
+			event.preventDefault();
+			$(this).replaceWith("<li>" + "<div>" + $(".itembox").val() + "</div>" + "<button class='delete'>Delete</button>" + "<button class='edit'>Edit</button>" + "</li>");
+		}); 
 	});
 	$(".todoList").on("click", ":checkbox", function (){
 		$(this).parent().toggleClass("completedItem");
@@ -22,6 +26,12 @@ $(document).ready(function(){
 
 
 
+/*
 
+PSEUDO
+1. PRESSING ENTER ON THE BOX NEEDS TO replace the box wih its value
+2. editItemBox needs to grab the value of the list item
+going to define a variable for the value of the parent li
+*/
 
 
