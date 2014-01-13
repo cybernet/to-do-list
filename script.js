24fbef237dccbfe8cc36a8130ee19f61cf9b0d44
MrDanMacDonald <dan@danmacd.com>
2014-01-13 05:37:26 +0000

    grouped buttons

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ $(document).ready(function(){
 		event.preventDefault();
 		var deleteButton = "<button class='delete btn btn-default'>Delete</button>";
 		var editButton = "<button class='edit btn btn-default'>Edit</button>";
-		$(".list_of_items").append("<li class='list-group-item'>" + "<div>" + $("#custom_textbox").val() + deleteButton + editButton + "<input type='checkbox'>" + "</div>" + "</li>");
+		var twoButtons = "<div class='btn-group'>" + deleteButton + editButton + "</div>"
+		$(".list_of_items").append("<li class='list-group-item'>" + "<div>" + $("#custom_textbox").val() + twoButtons + "<input type='checkbox'>" + "</div>" + "</li>");
 		$("#custom_textbox").val('');
 	});
 	$(".list_of_items").on("click", "button.delete", function(){
-		$(this).parent().parent().remove();
+		$(this).parent().parent().parent().remove();
 	});
 	$(".list_of_items").on("click", "button.edit", function (){
 		var editItemBox = "<form class='edit_input_box'><input type='text' class='itembox'></form>";
 		var originalItem = $(this).parent().val();
-		$(this).parent().replaceWith(editItemBox); /* FIXME */
+		$(this).parent().parent().replaceWith(editItemBox); /* FIXME */
 		$(".itembox").val(originalItem.val);
 		$("form.edit_input_box ").on("submit", function(){
-			event.preventDefault();
-			$(this).replaceWith("<div>" + $(".itembox").val() + "<button class='delete'>Delete</button>" + "<button class='edit'>Edit</button>" +  "<input type='checkbox'>" + "</div>");
+			event.preventDefault(); 
+			$(this).replaceWith("<div>" + $(".itembox").val() + "<button class='delete btn btn-default'>Delete</button>" + "<button class='edit btn btn-default'>Edit</button>" +  "<input type='checkbox'>" + "</div>");
 		}); 
 	});
 	$(".list_of_items").on("click", ":checkbox", function (){
