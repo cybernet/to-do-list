8a8f3f61af3769904b90b9a503f9e85874e751d2
MrDanMacDonald <dan@danmacd.com>
2014-02-16 22:51:01 +0000

    refactoring

--- script.js
+++ script.js
@@ -8,38 +8,29 @@ $(document).ready(function(){
 		$(".list_of_items").append("<li class='list-group-item'>" + "<div class='text_holder'>" + $("#custom_textbox").val() + twoButtons + "</div>" + checkBox + "</li>");
 		$("#custom_textbox").val('');
 	});
+
 	$(".list_of_items").on("click", "button.delete", function(){
-		$(this).parent().parent().parent().remove();
+		$(this).closest("li").remove();
 	});
+
 	$(".list_of_items").on("click", "button.edit", function (){
 		var editItemBox = "<form class='edit_input_box'><input type='text' class='itembox'></form>";
 		var originalItem = $(this).parent().val();
 		var deleteButton = "<button class='delete btn btn-warning'>Delete</button>";
 		var editButton = "<button class='edit btn btn-success'>Edit</button>";
 		var twoButtons = "<div class='btn-group pull-right'>" + deleteButton + editButton + "</div>";
-		$(this).parent().parent().replaceWith(editItemBox); 
-		$(".itembox").val(originalItem.val); /* FIXME */
+		$(this).closest("div.text_holder").replaceWith(editItemBox);
 		$("form.edit_input_box ").on("submit", function(){
 			event.preventDefault(); 
 			var checkBox = "<label><input type='checkbox'></label>";
 			$(this).replaceWith("<div>" + $(".itembox").val() + twoButtons + "</div>");
 		}); 
 	});
+	
 	$(".list_of_items").on("click", ":checkbox", function (){
-		$(this).parent().parent().parent().toggleClass("completed_item");
+		$(this).closest("li").toggleClass("completed_item");
 	});
 });
 
-
-
-
-
-/*
-
-not ordering list
-
-The code on line 20 does not work.  I am trying to get editItemBox to prepopulate with the text of its associated div.
-
-*/
 
 
